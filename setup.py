453ebba9bb3effd12f213d969e722dd87d40685d
Tayyeb <tayyeb@tsaze.com>
2017-03-25 23:07:04 +0430

    bump version to 1.4 and release to pypi as validate_email_wt

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(name='validate_email',
-      version = '1.3',
-      download_url = 'git@github.com:syrusakbary/validate_email.git',
-      py_modules = ('validate_email',),
-      author = 'Syrus Akbary',
-      author_email = 'me@syrusakbary.com',
-      description = 'validate_email verifies if an email address is valid and really exists.',
-      long_description=open('README.rst').read(),
-      keywords = 'email validation verification mx verify',
-      url = 'http://github.com/syrusakbary/validate_email',
-      license = 'LGPL',
-    )
+setup(
+    name='validate_email_wt',
+    version='1.4',
+    py_modules=('validate_email',),
+    author='w.Tayyeb',
+    author_email='w.tayyeb@gmail.com',
+    description='validate_email verifies if an email address is valid and really exists.',
+    long_description=open('README.rst').read(),
+    keywords='email validation verification mx verify',
+    url='https://github.com/wtayyeb/validate_email',
+    license='LGPL',
+)
